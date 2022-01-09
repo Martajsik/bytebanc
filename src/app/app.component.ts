ae259679649c421293c13a1aeb4aa7c20734d846
Marta J Silva <50676969+Martajsik@users.noreply.github.com>
2022-01-09 13:24:55 -0300

    styling of the transfer component, creation of the extract component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebanc';
+  transferencia: any;
+  valor!: number;
+  destino!: number;
+
+
+  transferir($event: any){
+    console.log($event);
+    this.transferencia = $event;
+    this.limparCampos();
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
